34cd8b7e695315318e3eecab4bfa2abccf5a98fb
Noel <noelbundick@gmail.com>
2022-11-11 18:46:49 +0000

    Pending changes exported from your codespace

---
+++ build/precache-manifest.feedcecf06fa735d80384ca91815af1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab1d071ee66371539be20f742091c1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f0dfe305b3b4edcbd98",
+    "url": "/static/css/2.8c1f6bf6.chunk.css"
+  },
+  {
+    "revision": "9f0dfe305b3b4edcbd98",
+    "url": "/static/js/2.948c040c.chunk.js"
+  },
+  {
+    "revision": "f0dbf673c578c48b2730",
+    "url": "/static/js/main.2f5625a8.chunk.js"
+  },
+  {
+    "revision": "07b663453590ba002ec8",
+    "url": "/static/js/runtime-main.5d7f14e0.js"
+  }
+]);
